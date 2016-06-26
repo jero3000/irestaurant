4043f8ae18e2c5a65acedc12646ec0212d6ff509
jero3000 <jero3000@gmai.com>
2016-06-26 13:13:51 +0200

    Initial model for Restaurant

---
+++ management/models/Restaurant.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from django.core.validators import RegexValidator
+from django.utils.encoding import python_2_unicode_compatible
+
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Nombre", blank=False)
+    address = models.CharField(max_length=200, verbose_name="Dirección")
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="El número de telefono debe tener el siguiente formato: '+999999999'. No más de 15 dígitos")
+    telephone = models.CharField(max_length=20, validators=[phone_regex], verbose_name="Telefono")
+
+    @python_2_unicode_compatible
+    def __str__(self):
+        return self.name
---
+++ management/models/__init__.py
@@ -0,0 +1,1 @@
+from Restaurant import Restaurant
