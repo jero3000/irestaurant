549f3f6a61865a43f861b92b876f018e3b6c7ede
jero3000 <jero3000@gmai.com>
2016-07-17 12:21:55 +0200

    Enable serving media files (only for testing)

--- irestaurant/urls.py
+++ irestaurant/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# TODO: Serving media files is not suitable for production use!
+# See: https://docs.djangoproject.com/en/1.9/howto/static-files/
