63d0399aa0b292f445bb2a73a468fbcf0c9cb745
jero3000 <jero3000@gmai.com>
2017-03-26 20:24:15 +0200

    management app moved to a subdirectory

--- manage.py
+++ manage.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "irestaurant.settings")
 
     from django.core.management import execute_from_command_line
+    from django.conf import settings
 
+    # Adding the "apps" directory to the sys path to avoid errors imporing apps when they are located in a subdirectory
+    sys.path.append(os.path.join(settings.BASE_DIR, "apps"))
     execute_from_command_line(sys.argv)
